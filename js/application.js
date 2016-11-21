f89e06076552bafc72d8b1e880e191530a1ceb71
Amanda Hinchey <akhinchey@gmail.com>
2016-11-21 16:29:31 -0500

    Add working js to application file

--- js/application.js
+++ js/application.js
@@ -1,4 +1,8 @@
-$(document).ready(function(){
+// $(document).ready(function() {
+
+// var line1 = $(".black-bishop")
+// var line2 = $(".white-bishop").parent();
+console.log($(".black-bishop"))
 
 function dragstart_handler(ev) {
  console.log("dragStart");
@@ -7,24 +11,52 @@ function dragstart_handler(ev) {
  var diags = $(ev.target).parent().attr("class").split(" ");
  var rightDiag = diags.pop();
  var leftDiag = diags.pop();
- // debugger;
- $("." + rightDiag).addClass("target");
+ var possibleMoves = $("." + rightDiag + ", ." + leftDiag)
+
+ if (ev.target.id.includes("black")) {
+    var opposingClass = "white-bishop";
+    var sameSquares = $(".black-bishop").parent();
+ } else {
+    var opposingClass = "black-bishop";
+    var sameSquares = $(".white-bishop").parent();
+ }
+
+ var opposingSquares = $("." + opposingClass).parent();
+ var classes = $.map(opposingSquares, function(obj) { return obj.className });
+
+ var finalClasses = [];
+
+ for (var i =0; i < classes.length; i++) { 
+    var x = classes[i].split(" "); 
+    finalClasses.push(x.pop()); 
+    finalClasses.push(x.pop()); 
+    }
+
+var classString = "." + finalClasses.join(", .")
+var blockedCells = $(classString);
+
+var finalMoves = $.grep( possibleMoves, function(cell) { return $.inArray(cell, blockedCells) < 0; });
+
+$(finalMoves).addClass("target")
+$(sameSquares).removeClass("target")
+  // debugger;
+
  ev.effectAllowed = "move";
 }
+
 function dragover_handler(ev) {
  console.log("dragOver");
- // ev.currentTarget.style.background = "lightblue";
  ev.preventDefault();
 }
+
 function drop_handler(ev) {
   console.log("Drop");
   ev.preventDefault();
   var id = ev.dataTransfer.getData("text");
   var className = $(document.getElementById(id)).attr("class")
-  // debugger;
   if ( $(ev.target).attr("class").includes("target") )
     ev.target.appendChild(document.getElementById(id));
-
+    $(".target").removeClass("target");
 }
 
 function dragend_handler(ev) {
@@ -33,33 +65,6 @@ function dragend_handler(ev) {
   ev.dataTransfer.clearData();
 }
 
-});
 
+// });
 
-
-
-
-
-
-  // $(".cell").click( function(event) {
-  //   // debugger;
-  //   var selectClasses = $(this).attr("class").split(" ");
-  //   var selectRightDiagonal = selectClasses.pop();
-  //   var selectLeftDiagonal = selectClasses.pop();
-  //   var selectRow = selectClasses.pop();
-  //   var selectCol = selectClasses.pop();
-
-  //   $("." + selectRightDiagonal).children(".queen").removeClass("active").fadeOut();
-  //   $("." + selectLeftDiagonal).children(".queen").removeClass("active").fadeOut();
-  //   $("." + selectRow).children(".queen").removeClass("active").fadeOut();
-  //   $("." + selectCol).children(".queen").removeClass("active").fadeOut();
-
-  //   $(this).children(".queen").addClass("active").fadeIn();
-
-  //   if ( $(".active").length === 8 ) {
-  //     $(".winner-message").show();
-  //   } else {
-  //     $(".winner-message").hide();
-  //   }
-
-  // })
